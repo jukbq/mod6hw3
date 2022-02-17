36d8b98a08846530b5af76496f0ec261c6b23907
Yurii Ohlii <jukbq77@gmail.com>
2022-02-17 14:42:30 +0200

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,32 +9,58 @@ import { convertTypeAcquisitionFromJson } from 'typescript';
 export class AppComponent {
   title = 'mod6hw3';
   public text = ''
+  public editText = ''
   public taskArr: any = [];
-  public taskNumb: number =  0
+  public taskNumb: number = 0
   public index!: number;
+  public edirIndex!: number;
+  public tempOn: boolean = false;
 
   add() {
     let task = {
-      taskName: this.text,
+      taskName: '',
       status: false
     }
+    task.taskName = this.text
     this.taskArr.push(task);
     this.text = ''
     for (let i = 0; i < this.taskArr.length; i++) {
       this.taskNumb = i + 1
     }
+  }
+
+  indexElement(data: number) {
+    this.index = data;
+    console.log(this.index);
+
+  }
+  changeStatus(data: any) {
+    let task = this.taskArr[this.index];
+    task.status = data;
+  }
+
+
+  openEdit(data: boolean) {
+    this.tempOn = data
+    let taskId = this.taskArr[this.index]
+    this.editText = taskId.taskName
+    console.log(taskId);
+  }
+
+
+
+  dellTask(data: number) {
+    this.taskArr.splice(data, 1)
+  }
+
+
+  save(): void {
+    let i = this.index;
+    this.taskArr[i].taskName = this.editText
+    this.tempOn = false
 
 
   }
-
-indexElement(data: number){
-  this.index = data;
-}
-
-changeStatus(data: any){
-    let task = this.taskArr[this.index];
-    task.status = data;
-}
 
 
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,6 @@
 import { style } from '@angular/animations';
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { convertTypeAcquisitionFromJson } from 'typescript';
 
 
 @Component({
@@ -8,67 +9,41 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-  @Input() arr! : any;
+  @Input() arr!: any;
+  @Input() editBtn!: any;
   @Output() index = new EventEmitter<any>();
   @Output() status = new EventEmitter<any>();
+  @Output() editTask = new EventEmitter<any>();
+  @Output() delete = new EventEmitter<any>();
 
-  public statusName ='IN PROGRESS';;
-  public isColor = false
-  public delStatus = true
+  public openEdit = false;
+  public editStatus = this.editBtn;
+
+  constructor() { };
+
+  ngOnInit(): void { };
 
 
-/* @Input()
-  set taskStatus(name: any){
-    if(name === true){
-    this.statusName = 'Done';
-    this.isColor = true;
-    this.delStatus = false
-  }
-  else{
-    this.statusName ='IN PROGRESS';
-    this.isColor = false;
-    this.delStatus = true
-  }
-} */
-  
- 
+  changeStatus($event: any) {
+    const id = $event.target.value;
+    const check = $event.target.checked;
+    this.index.emit(id);
+    this.status.emit(check);
+  };
 
 
 
+  edit($event: any): void {
+    const id = $event.target.value;
+    this.openEdit = true;
+    this.index.emit(id);
+    this.editTask.emit(this.openEdit);
 
-  ngOnInit(): void {
-
-  }
+  };
 
 
-  onChangeStatus($event: any) {
-      const id = $event.target.value; 
-      const check = $event.target.checked
-   
-      this.index.emit(id)
-      this.status.emit(check)
-      console.log(id);
-      console.log(check);
-      console.log(this.arr);
-    
-
-     if(check === true){
-    this.statusName = 'Done';
-    this.isColor = true;
-    this.delStatus = false
-  }
-  else{
-    this.statusName ='IN PROGRESS';
-    this.isColor = false;
-    this.delStatus = true
+  del(index: number): void {
+    this.delete.emit(index);
   }
 
-    
-  }
-
-
-
-  edit(): void {}
-  del() { }
-
-}
+};
